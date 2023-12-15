133602d735ee7cec3bb4ed4f79148a637aff0585
Alan Paulo <alanpaullo@yahoo.com.br>
2023-12-14 23:47:33 -0300

    alteraçãode projeto

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# AplicationWebSpringBoot
+# Aplicação Agenda Show
 
-To start the project in the database it is necessaryto open the Xampp server
-and start Apache and Mysql.
-- Start the application using Spring Boot
-- Start the database in Mysql
+Para executar o Projeto é necessário que tenha:
+
+Instalado:
+- Xampp
+- MySQL
+
+Para executar o projeto:
+
+- Start Xampp em modulo Mysql
+- Start conexão banco
+   - porta 3306
+   - user "root"
+   - Password ""
+- Start Spring Boot
+
+Para acessar aplicação web:
+
+- http://localhost:8080/agendaShow
+
+Para acessar o Swagger dos endpoints da aplicação:
+
+- http://localhost:8080/swagger-ui.html
+
+# Medium
+
+Aqui neste [Link](https://medium.com/@alanpaulodejesus/inicializando-e-criando-uma-aplica%C3%A7%C3%A3o-web-java-com-spring-boot-62652a4484ad) possui um passo a passo de como foi criado este projeto.
---
+++ agendaShowEventos/src/test/java/agendaShowEventos/AgendaShowTestWeb.java
@@ -0,0 +1,29 @@
+package agendaShowEventos;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AgendaShowTestWeb {
+
+    public AgendaShowTestWeb() {
+        super();
+    }
+    @Test
+    public void validarRedirecionamentoAgendaShow() {
+        System.setProperty("webdriver.chrome.driver", "src/test/java/agendaShowEventos/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080/agendas");
+        WebElement agenda=driver.findElement(By.id("btn-retornar-nova-agenda"));
+        agenda.click();
+        String actualUrl="http://localhost:8080/agendaShow";
+        String expectedUrl= driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl,actualUrl);
+        driver.quit();
+    }
+}
